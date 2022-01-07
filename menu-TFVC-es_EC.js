5e3aaeb87ad566eba7822e38ddd3a677bfa3521d
Tony Veas <tveascervantes@hotmail.com>
2022-01-06 22:22:55 -0500

    Se agrega menú de ejemplo

--- menu-TFVC-es_EC.js
+++ menu-TFVC-es_EC.js
@@ -2,7 +2,8 @@
     "COMMONS": {
         "MENU": {
 			"MNU_SMCMC":"Formularios Semillero",
-			"MNU_EJEMPLO":"Ejemplo"
+			"MNU_EJEMPLO":"Ejemplo",
+			"MNU_EJEMPLO2":"Ejemplo2"
         }
     }
 }
