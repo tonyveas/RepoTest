b55c6d693888844c2376e1a34b20031fb66ca8ac
Tony Veas <tveascervantes@hotmail.com>
2022-01-06 22:32:03 -0500

    menú 5

--- menu-TFVC-es_EC.js
+++ menu-TFVC-es_EC.js
@@ -3,7 +3,9 @@
         "MENU": {
 			"MNU_SMCMC":"Formularios Semillero",
 			"MNU_EJEMPLO":"Ejemplo",
-			"MNU_EJEMPLO2":"Ejemplo2"
+			"MNU_EJEMPLO2":"Ejemplo2",
+			"MNU_EJEMPLO5":"Ejemplo5",
+			"MNU_EJEMPLO6":"Menu 6"
         }
     }
 }
